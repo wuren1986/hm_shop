8bbf13e0037ac54251dbf7a82b5bb31f0953f8c8
ren_wu <562117904@qq.com>
2026-05-11 01:33:35 +0800

    登录页面搭建和表单校验

--- lib/pages/Login/index.dart
+++ lib/pages/Login/index.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:hm_shop/utils/ToastUtils.dart';
 
 class LoginPage extends StatefulWidget {
   LoginPage({Key? key}) : super(key: key);
@@ -8,11 +9,186 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
+  TextEditingController _phoneController = TextEditingController(); // 账号控制器
+  TextEditingController _codeController = TextEditingController(); // 密码控制器
+  // 用户账号Widget
+  Widget _buildPhoneTextField() {
+    return TextFormField(
+      // 配合Form组件可以实现表单校验
+      validator: (value) {
+        // 表单校验
+        if (value == null || value.isEmpty) {
+          return '账号不能为空';
+        }
+        if (!RegExp(r'^1[3-9]\d{9}$').hasMatch(value)) {
+          return '请输入正确的手机号';
+        }
+        return null;
+      },
+      controller: _phoneController,
+      decoration: InputDecoration(
+        contentPadding: EdgeInsets.only(left: 20), // 内容内边距
+        hintText: "请输入账号",
+        fillColor: const Color.fromRGBO(243, 243, 243, 1),
+        filled: true,
+        border: OutlineInputBorder(
+          borderSide: BorderSide.none,
+          borderRadius: BorderRadius.circular(25),
+        ),
+      ),
+    );
+  }
+
+  // 用户密码Widget
+  Widget _buildCodeTextField() {
+    return TextFormField(
+      // 配合Form组件可以实现表单校验
+      validator: (value) {
+        // 表单校验
+        if (value == null || value.isEmpty) {
+          return '密码不能为空';
+        }
+        // 密码校验 6-16位的数字字母或下划线
+        if (!RegExp(r'^[a-zA-Z0-9_]{6,16}$').hasMatch(value)) {
+          return '请输入6-16位的数字字母或下划线';
+        }
+        return null;
+      },
+      controller: _codeController,
+      obscureText: true, // 隐藏密码
+      decoration: InputDecoration(
+        contentPadding: EdgeInsets.only(left: 20), // 内容内边距
+        hintText: "请输入密码",
+        fillColor: const Color.fromRGBO(243, 243, 243, 1),
+        filled: true,
+        border: OutlineInputBorder(
+          borderSide: BorderSide.none,
+          borderRadius: BorderRadius.circular(25),
+        ),
+      ),
+    );
+  }
+
+  // 登录按钮Widget
+  Widget _buildLoginButton() {
+    return SizedBox(
+      width: double.infinity,
+      height: 50,
+      child: ElevatedButton(
+        onPressed: () {
+          // 登录逻辑
+          if (_formKey.currentState!.validate()) {
+            // 对复选框状态进行校验
+            if (!_isChecked) {
+              // 提示用户勾选用户协议
+              ToastUtils.showToast(context, "请先同意隐私条款和用户协议");
+              return;
+            }
+            // 登录逻辑
+            ToastUtils.showToast(context, "登录成功");
+          }
+        },
+        style: ElevatedButton.styleFrom(
+          backgroundColor: Colors.black,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(25),
+          ),
+        ),
+        child: Text("登录", style: TextStyle(fontSize: 18, color: Colors.white)),
+      ),
+    );
+  }
+
+  bool _isChecked = false;
+  // 勾选Widget
+  Widget _buildCheckbox() {
+    return Row(
+      children: [
+        // 设置勾选为圆角
+        Checkbox(
+          value: _isChecked,
+          activeColor: Colors.black,
+          checkColor: Colors.white,
+          onChanged: (bool? value) {
+            // 切换勾选状态
+            setState(() {
+              _isChecked = value ?? false;
+            });
+          },
+          // 设置形状
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(10), // 圆角大小
+          ),
+          // 可选：设置边框
+          side: BorderSide(color: Colors.grey, width: 2.0),
+        ),
+        Text.rich(
+          TextSpan(
+            children: [
+              TextSpan(text: "查看并同意"),
+              TextSpan(
+                text: "《隐私条款》",
+                style: TextStyle(color: Colors.blue),
+              ),
+              TextSpan(text: "和"),
+              TextSpan(
+                text: "《用户协议》",
+                style: TextStyle(color: Colors.blue),
+              ),
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+
+  // 头部Widget
+  Widget _buildHeader() {
+    return Row(
+      children: [
+        Padding(
+          padding: EdgeInsets.only(left: 10),
+          child: Text(
+            "账号密码登录",
+            style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+          ),
+        ),
+      ],
+    );
+  }
+
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('登录'), centerTitle: true),
-      body: Center(child: Text('登录页面')),
+      appBar: AppBar(
+        title: Text("惠多美登录", style: TextStyle(fontWeight: FontWeight.bold)),
+        centerTitle: true,
+        backgroundColor: Colors.white,
+      ),
+      body: Form(
+        // 表单组件
+        key: _formKey, // 绑定GlobalKey，用于校验表单数据
+        // 表单校验
+        child: Container(
+          padding: EdgeInsets.all(30),
+          color: Colors.white,
+          child: Column(
+            children: [
+              SizedBox(height: 20),
+              _buildHeader(),
+              SizedBox(height: 30),
+              _buildPhoneTextField(),
+              SizedBox(height: 20),
+              _buildCodeTextField(),
+              SizedBox(height: 20),
+              _buildCheckbox(),
+              SizedBox(height: 20),
+              _buildLoginButton(),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
--- lib/utils/ToastUtils.dart
+++ lib/utils/ToastUtils.dart
@@ -1,17 +1,30 @@
 import 'package:flutter/material.dart';
 
 class ToastUtils {
+  // 控制显示状态
+  static bool showLoading = false; // 是否正在显示提示提示
+
   static void showToast(BuildContext context, String? message) {
+    // 限制当前只能显示一个提示
+    if (ToastUtils.showLoading) {
+      return;
+    }
+    ToastUtils.showLoading = true;
+    Future.delayed(Duration(seconds: 3), () {
+      ToastUtils.showLoading = false;
+    });
+
+    // 显示提示
     ScaffoldMessenger.of(context).showSnackBar(
       SnackBar(
         // 提示消息
-        width: 120,
+        width: 180,
         shape: RoundedRectangleBorder(
           // 提示框圆角
           borderRadius: BorderRadius.circular(40),
         ),
         behavior: SnackBarBehavior.floating, // 浮动显示
-        duration: Duration(seconds: 5), // 显示时长
+        duration: Duration(seconds: 3), // 显示时长
         content: Text(message ?? "加载成功", textAlign: TextAlign.center),
       ),
     );
